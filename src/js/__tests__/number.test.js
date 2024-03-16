dc7de177fd91037be0f2a94b214d8c9c5be060f7
sleeper-g <green.sleeper1@gmail.com>
2024-03-16 23:56:53 +0300

    add correction

--- src/js/__tests__/number.test.js
+++ src/js/__tests__/number.test.js
@@ -2,13 +2,13 @@ import convertN from './../number.js'
 
 test('phone 1', () => {
   const phone = '8 (927) 000-00-00';
-  expect(convertN()).toBe('+79270000000')
+  expect(convertN(phone)).toBe('+79270000000')
 })
 test('phone 2', () => {
   const phone = '+7 960 000 00 00';
-  expect(convertN()).toBe('+79600000000')
+  expect(convertN(phone)).toBe('+79600000000')
 })
 test('phone 3', () => {
   const phone = '+86 000 000 0000';
-  expect(convertN()).toBe('+860000000000')
+  expect(convertN(phone)).toBe('+860000000000')
 })
--- src/js/number.js
+++ src/js/number.js
@@ -1,8 +1,8 @@
 export default function convertN(phoneNumber) {
-  if (phoneNumber.search(/^(\+7|\+86)/)){
+  if (phoneNumber.search(/^(\+7|\+86)/) !== -1){
     return phoneNumber.replace(/ /g, '')
   }
-  if (phoneNumber.search(/^8 \(\d\d\d\)/)){
+  if (phoneNumber.search(/^8 \(\d\d\d\)/) !== -1){
     return phoneNumber.replace(/^8/, '+7').replace(/[()\- ]/g, '')
-}
+  }
 }
